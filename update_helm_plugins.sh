dc67cdfbca6f6c744569d97795a854bfbbd38fc9
sdelrio <oftheriver@gmail.com>
2021-04-03 16:12:34 +0200

    fix: minor fixes on upate helm plugin script

--- update_helm_plugins.sh
+++ update_helm_plugins.sh
@@ -5,14 +5,14 @@ get_latest_release() {
 # get_latest_release user/project
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub API
     grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"v([^"]+)".*/\1/'                                    # Pluck JSON value
+    sed -E 's/.*"v([^"]+)".*/\1/'                                   # Pluck JSON value
 }
 
 update_dockerfile() {
 # Usage
 # update_dockerfile ENVVAR VALUE
   if egrep -q "ARG $1=\"$2\"" Dockerfile; then
-    echo "Update of $1=$2 not needed"
+    echo "Update on $1=$2 not needed"
   else
     echo "Updating $1=$2"
     sed -i "s/ARG $1=.*/ARG $1=\"$2\"/" Dockerfile
@@ -39,11 +39,12 @@ VERSIONS=(
 counter=0
 for p in ${PLUGINS[@]}; do 
     ENVVAR=${VERSIONS[$counter]}
-#    echo $p  $ENVVAR
     RELEASE=$(get_latest_release ${p})
+
     if [ ! -z "${RELEASE}" ]; then
         echo ${p} latest release ${RELEASE}
         update_dockerfile ${ENVVAR} ${RELEASE}
     fi
+
     ((counter=counter + 1))
 done
